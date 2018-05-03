865e62d6aacd7abcbe687c679032331c2b74ec4a
Beomseok Oh <beomseoks.oh@samsung.com>
2018-05-03 15:25:19 +0900

    Fix public API to private

--- java/src/main/java/org/datamodel/aml/Representation.java
+++ java/src/main/java/org/datamodel/aml/Representation.java
@@ -26,17 +26,19 @@ public final class Representation {
     }
 
     /**
-     * @brief       Constructor.
+     * Constructor for Representation with aml file path.
+     * 
      * @param       amlFilePath [in] File path of AML that contains a data model information.
      */
     public Representation(String amlFilePath) throws AMLException {
         this.mNativeHandle = constructRepresentation(amlFilePath);
     }
 
-    public native long constructRepresentation(String amlFilePath) throws AMLException;
+    private native long constructRepresentation(String amlFilePath) throws AMLException;
 
     /**
-     * @brief       This function converts AMLObject to AML(XML) string to match the AML model information which is set by constructor.
+     * This function converts AMLObject to AML(XML) string to match the AML model information which is set by constructor.
+     * 
      * @param       amlObject [in] AMLObject to be converted.
      * @return      AML(XML) string converted from amlObject.
      * @exception   AMLException If the schema of amlObject does not match to AML model information
@@ -48,7 +50,8 @@ public final class Representation {
     private native String DataToAmlImpl(AMLObject amlObject) throws AMLException;
 
     /**
-     * @brief       This function converts AML(XML) string to AMLObject to match the AML model information which is set by constructor.
+     * This function converts AML(XML) string to AMLObject to match the AML model information which is set by constructor.
+     * 
      * @param       xmlStr [in] AML(XML) string to be converted.
      * @return      AMLObject instance converted from AML(XML) string.
      * @exception   AMLException If the schema of xmlStr does not match to AML model information
@@ -60,11 +63,12 @@ public final class Representation {
     private native AMLObject AmlToDataImpl(String xmlStr) throws AMLException;
 
     /**
-     * @brief       This function converts AMLObject to Protobuf byte data to match the AML model information which is set by constructor.
+     * This function converts AMLObject to Protobuf byte data to match the AML model information which is set by constructor.
+     * If 'disable_protobuf' build option is enabled, this API will be DISABLED and throw AMLException with code 'API_NOT_ENABLED'.
+     * 
      * @param       amlObject [in] AMLObject to be converted.
      * @return      Protobuf byte data converted from amlObject.
      * @exception   AMLException If the schema of amlObject does not match to AML model information
-     * @note        If 'disable_protobuf' build option is enabled, this API will be DISABLED and throw AMLException with code 'API_NOT_ENABLED'.
      */
     public byte[] DataToByte(AMLObject amlObject) throws AMLException {
         return this.DataToByteImpl(amlObject);
@@ -73,11 +77,12 @@ public final class Representation {
     private native byte[] DataToByteImpl(AMLObject amlObject) throws AMLException;
 
     /**
-     * @brief       This function converts Protobuf byte data to AMLObject to match the AML model information which is set by constructor.
-     * @param       byte [in] Protobuf byte data to be converted.
+     * This function converts Protobuf byte data to AMLObject to match the AML model information which is set by constructor.
+     * If 'disable_protobuf' build option is enabled, this API will be DISABLED and throw AMLException with code 'API_NOT_ENABLED'.
+     * 
+     * @param       amlByte [in] Protobuf byte data to be converted.
      * @return      AMLObject instance converted from amlObject.
      * @exception   AMLException If the schema of byte does not match to AML model information
-     * @note        If 'disable_protobuf' build option is enabled, this API will be DISABLED and throw AMLException with code 'API_NOT_ENABLED'.
      */
     public AMLObject ByteToData(byte[] amlByte) throws AMLException {
         return this.ByteToDataImpl(amlByte);
@@ -86,7 +91,8 @@ public final class Representation {
     private native AMLObject ByteToDataImpl(byte[] amlByte) throws AMLException;
 
     /**
-     * @brief       This function returns AutomationML SystemUnitClassLib's unique ID
+     * This function returns AutomationML SystemUnitClassLib's unique ID
+     * 
      * @return      string value of AML SystemUnitClassLIb's ID
      */
     public String getRepresentationId() throws AMLException {
@@ -96,7 +102,8 @@ public final class Representation {
     private native String getRepresentationIdImpl() throws AMLException;
 
     /**
-     * @brief       This function returns AMLObject that contains configuration data which is present in RoleClassLib.
+     * This function returns AMLObject that contains configuration data which is present in RoleClassLib.
+     * 
      * @return      AMLObject that has configuration data.
      */
     public AMLObject getConfigInfo() throws AMLException {
